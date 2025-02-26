6876cb213f7f71f029747a96ad63942334eb7c8d
Adil Ahmed <adilahmed749@gmail.com>
2025-02-26 16:30:45 +0000

    Adding fetch functions

--- pokemon-generator/src/main.ts
+++ pokemon-generator/src/main.ts
@@ -2,15 +2,11 @@ import './style.css'
 
 const form = document.getElementById("pokemonForm") as HTMLFormElement;
 
-
-fetchAllPokemonData();
-
-
-
+// fetchAllPokemonData();
 
 // Handle form submission
 form.addEventListener("submit", async (event) => {
-  event.preventDefault
+  event.preventDefault()
 
   const userName = (document.getElementById("userName") as HTMLInputElement).value;
 
@@ -21,17 +17,28 @@ form.addEventListener("submit", async (event) => {
   if (pokemon) {
     pokemonName.textContent = pokemon.name;
     pokemonType.textContent = `Type: ${pokemon.type}`;
-    
+    pokemonImage.src = pokemon.image;
   }
 
+  addToTable(userName, pokemon.name, pokemon.type, pokemon.image);
+
+  // send pokemon and username to the db
 
 });
 
 // Fetch all the pokemon data
-
+const fetchAllPokemonData = async () => {
+  const response = await fetch("https://example.com");
+  const data = await response.json();
+};
 
 // Fetch a random pokemon from the backend
+// const fetchRandomPokemon() = async () => {
+//   const response = await fetch("https://example.com");
+//   const data = await response.json();
+// };
 
 
-// logic to update table row
-// Add another empty row
+// Logic to update the table with specific entry
+  // create user pokemon function
+  // Add another empty row
