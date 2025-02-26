237342bb7c05d1eaf559c70bf50edd3e9b53f726
JayBains <jayarjunbains@gmail.com>
2025-02-26 12:49:58 +0000

    Created PokemonController class to map get requests to specific URL

---
+++ src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
@@ -0,0 +1,25 @@
+package com.example.pokemon_generator.Controllers;
+
+import com.example.pokemon_generator.Models.Pokemon;
+import com.example.pokemon_generator.Services.PokemonService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/pokemon")
+public class PokemonController {
+    private final PokemonService pokemonService;
+
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping("/")
+    public Pokemon getPokemon() {
+        Random random = new Random();
+        return pokemonService.getPokemonByName(String.valueOf(random.nextInt(151)));
+    }
+}
