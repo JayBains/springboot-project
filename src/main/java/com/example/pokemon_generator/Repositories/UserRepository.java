812b2e46d25e303d082ea80e115a9b40f947add2
Elias Bennett <eliasthebennett@gmail.com>
2025-02-26 22:23:44 +0000

    Modifying the PokemonRepository to use the JdbcTemplate for raw SQL Queries

--- src/main/java/com/example/pokemon_generator/Repositories/UserRepository.java
+++ src/main/java/com/example/pokemon_generator/Repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package com.example.pokemon_generator.Repositories;
 
-import com.example.pokemon_generator.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void insertUser(String username) {
+        String sql = "INSERT INTO users (username) VALUES (?) ON DUPLICATE KEY UPDATE username = username";
+        jdbcTemplate.update(sql, username);
+    }
+
+    public Integer getUserIdByUsername(String username) {
+        String sql = "SELECT id FROM users WHERE username = ?";
+        return jdbcTemplate.queryForObject(sql, Integer.class, username);
+    }
 }
