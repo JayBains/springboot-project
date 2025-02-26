002d00af997e0e814d92eff2978d893a415fe09e
JayBains <jayarjunbains@gmail.com>
2025-02-26 13:25:57 +0000

    Added ability to search by species/id

--- src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
+++ src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
@@ -3,9 +3,11 @@ package com.example.pokemon_generator.Controllers;
 import com.example.pokemon_generator.Models.Pokemon;
 import com.example.pokemon_generator.Services.PokemonService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.Random;
 
 @RestController
@@ -20,6 +22,11 @@ public class PokemonController {
     @GetMapping("/")
     public Pokemon getPokemon() {
         Random random = new Random();
-        return pokemonService.getPokemonByName(String.valueOf(random.nextInt(151)));
+        return pokemonService.getPokemonByName(String.valueOf(random.nextInt(150) + 1));
+    }
+
+    @GetMapping("/{ID}")
+    public Pokemon getPokemonByID(@PathVariable String ID) {
+        return pokemonService.getPokemonByName(ID);
     }
 }
