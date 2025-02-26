62e4247728a324fa72ac251a83cf146096c3ed3f
Elias Bennett <eliasthebennett@gmail.com>
2025-02-26 22:56:00 +0000

    Modifying PokemonController to also store the pokemon in MySQL, link to the user, and keep track of which user generated which pokemon.

--- src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
+++ src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
@@ -2,13 +2,7 @@ package com.example.pokemon_generator.Controllers;
 
 import com.example.pokemon_generator.Models.Pokemon;
 import com.example.pokemon_generator.Services.PokemonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
-import java.util.Random;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pokemon")
@@ -19,14 +13,45 @@ public class PokemonController {
         this.pokemonService = pokemonService;
     }
 
-    @GetMapping("/")
-    public Pokemon getPokemon() {
-        Random random = new Random();
-        return pokemonService.getPokemonByName(String.valueOf(random.nextInt(150) + 1));
-    }
-
-    @GetMapping("/{ID}")
-    public Pokemon getPokemonByID(@PathVariable String ID) {
-        return pokemonService.getPokemonByName(ID);
+    @PostMapping("/generate")
+    public Pokemon generatePokemon(@RequestParam String username) {
+        return pokemonService.generatePokemon(username);
     }
 }
+
+
+
+
+//Commenting out the original code in case we need to revert
+//package com.example.pokemon_generator.Controllers;
+//
+//import com.example.pokemon_generator.Models.Pokemon;
+//import com.example.pokemon_generator.Services.PokemonService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Optional;
+//import java.util.Random;
+//
+//@RestController
+//@RequestMapping("/pokemon")
+//public class PokemonController {
+//    private final PokemonService pokemonService;
+//
+//    public PokemonController(PokemonService pokemonService) {
+//        this.pokemonService = pokemonService;
+//    }
+//
+//    @GetMapping("/")
+//    public Pokemon getPokemon() {
+//        Random random = new Random();
+//        return pokemonService.getPokemonByName(String.valueOf(random.nextInt(150) + 1));
+//    }
+//
+//    @GetMapping("/{ID}")
+//    public Pokemon getPokemonByID(@PathVariable String ID) {
+//        return pokemonService.getPokemonByName(ID);
+//    }
+//}
