a8e5f9fb25681e661b3d908d535e46827cf23c2b
Adil Ahmed <adilahmed749@gmail.com>
2025-02-26 15:09:13 +0000

    Adding initial TS logic including a DOM element for form and and event handler

--- pokemon-generator/src/main.ts
+++ pokemon-generator/src/main.ts
@@ -1,1 +1,37 @@
 import './style.css'
+
+const form = document.getElementById("pokemonForm") as HTMLFormElement;
+
+
+fetchAllPokemonData();
+
+
+
+
+// Handle form submission
+form.addEventListener("submit", async (event) => {
+  event.preventDefault
+
+  const userName = (document.getElementById("userName") as HTMLInputElement).value;
+
+  // Fetch a random pokemon from the backend
+  const pokemon = fetchRandomPokemon(userName);
+
+  // Logic to fill pokemon form && clear at the end
+  if (pokemon) {
+    pokemonName.textContent = pokemon.name;
+    pokemonType.textContent = `Type: ${pokemon.type}`;
+    
+  }
+
+
+});
+
+// Fetch all the pokemon data
+
+
+// Fetch a random pokemon from the backend
+
+
+// logic to update table row
+// Add another empty row
