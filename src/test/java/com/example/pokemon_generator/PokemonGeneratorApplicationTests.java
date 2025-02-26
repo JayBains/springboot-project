1264a3ae0647991f6be036d99265e1a06d91cece
JayBains <jayarjunbains@gmail.com>
2025-02-26 09:21:18 +0000

    Initialised Spring Boot project

---
+++ src/test/java/com/example/pokemon_generator/PokemonGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
