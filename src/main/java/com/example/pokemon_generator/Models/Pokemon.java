3933bed01ff5182a622559132790e026696912b6
JayBains <jayarjunbains@gmail.com>
2025-02-26 15:31:22 +0000

    PokemonService now grabs url of the pokemon front image

--- src/main/java/com/example/pokemon_generator/Models/Pokemon.java
+++ src/main/java/com/example/pokemon_generator/Models/Pokemon.java
@@ -2,9 +2,11 @@ package com.example.pokemon_generator.Models;
 
 public class Pokemon {
     private String name;
+    private String spriteUrl;
 
-    public Pokemon(String name){
+    public Pokemon(String name, String spriteUrl){
         this.name = name;
+        this.spriteUrl = spriteUrl;
     }
 
     public Pokemon(){}
@@ -15,5 +17,13 @@ public class Pokemon {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSpriteUrl() {
+        return spriteUrl;
+    }
+
+    public void setSpriteUrl(String spriteUrl) {
+        this.spriteUrl = spriteUrl;
     }
 }
--- src/main/java/com/example/pokemon_generator/Services/PokemonService.java
+++ src/main/java/com/example/pokemon_generator/Services/PokemonService.java
@@ -1,8 +1,11 @@
 package com.example.pokemon_generator.Services;
 
 import com.example.pokemon_generator.Models.Pokemon;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class PokemonService {
@@ -11,6 +14,19 @@ public class PokemonService {
 
     public Pokemon getPokemonByName(String name) {
         String url = "https://pokeapi.co/api/v2/pokemon/" + name;
-        return restTemplate.getForObject(url, Pokemon.class);
+
+        // Get the full response
+        ResponseEntity<Map> responseEntity = restTemplate.getForEntity(url, Map.class);
+        Map pokemonData = responseEntity.getBody();
+
+        // Extract only what we need
+        Pokemon simplePokemon = new Pokemon();
+        simplePokemon.setName((String) pokemonData.get("name"));
+
+        // Extract the front_default sprite URL
+        Map<String, Object> sprites = (Map<String, Object>) pokemonData.get("sprites");
+        simplePokemon.setSpriteUrl((String) sprites.get("front_default"));
+
+        return simplePokemon;
     }
 }
