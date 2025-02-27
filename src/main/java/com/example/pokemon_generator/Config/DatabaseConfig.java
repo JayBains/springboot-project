0c5564e85ab8b0428e09eb5038bd6d09a0fbf0ec
Elias Bennett <eliasthebennett@gmail.com>
2025-02-27 00:49:31 +0000

    Fixed database connection and API integration.

---
+++ src/main/java/com/example/pokemon_generator/Config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.example.pokemon_generator.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://crossover.proxy.rlwy.net:25488/railway");
+        dataSource.setUsername("root");
+        dataSource.setPassword("GPXInybeOZQHeyPBvGiqRHpBLynHLxRA");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
+
