7050ca4dfc504013805e8520a2aebc9f0b136558
Elias Bennett <eliasthebennett@gmail.com>
2025-02-28 14:52:20 +0000

    Updating PokemonController with CORS to allow local host connection.

--- backend/src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
+++ backend/src/main/java/com/example/pokemon_generator/Controllers/PokemonController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonController {
